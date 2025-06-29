6718c0eb9390db89425cdf40a78ca380117b56f6
Powik174 <84268704+Powik174@users.noreply.github.com>
2025-06-30 02:24:24 +0300

    Report

--- modules/model.py
+++ modules/model.py
@@ -3,7 +3,7 @@ from numpy.random import permutation
 
 
 def random_permutations(n, count):
-    return [permutation(n) for i in range(count)]
+    return [list(map(int, permutation(n))) for i in range(count)]
 
 
 def place_rectangles(order, rects, width):
@@ -45,3 +45,40 @@ class StripPackingGenAlg:
     def suitability(self, order):
         length = place_rectangles(order)[0]
         return 1 / length
+
+    def crossover(self, gene1, gene2):
+        n = len(gene1)
+        p1, p2 = random.randint(0, n - 1), random.randint(0, n - 1)
+        p1, p2 = min(p1, p2), max(p1, p2)
+        gene_new1, gene_new2 = [-1] * n, [-1] * n
+        for i in range(p1, p2 + 1):
+            gene_new1[i], gene_new2[i] = gene2[i], gene1[i]
+        gene1_cut = set(gene1[p1:p2 + 1])
+        gene2_cut = set(gene2[p1:p2 + 1])
+        i, j = 0, 0
+        while i < n:
+            if gene_new1[i] == -1:
+                if gene1[j] not in gene2_cut:
+                    gene_new1[i] = gene1[j]
+                j += 1
+            else:
+                i += 1
+        i, j = 0, 0
+        while i < n:
+            if gene_new2[i] == -1:
+                if gene2[j] not in gene1_cut:
+                    gene_new2[i] = gene2[j]
+                j += 1
+            else:
+                i += 1
+        print(p1, p2)
+        return gene_new1, gene_new2
+
+    def mutation(self, gene):
+        n = len(gene)
+        p1, p2 = random.randint(0, n - 1), random.randint(0, n - 1)
+        while p1 == p2:
+            p2 = random.randint(0, n - 1)
+        gene_new = gene.copy()
+        gene_new[p1], gene_new[p2] = gene_new[p2], gene_new[p1]
+        return gene_new
